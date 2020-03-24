8bffe0534eb8677384a802e03044510d01dd6c67
weinbergerlab <dweinber@gmail.com>
2020-03-23 23:47:06 -0400

    init package

---
+++ R/functions.R
@@ -0,0 +1,13 @@
+#Format line list data into time series
+line_list_format<-function(ds, syndromes,resolution='day'){
+  
+}
+
+excess_cases<-function(){
+  
+}
+
+syndromic_dashboard<-function(){
+  
+}
+
