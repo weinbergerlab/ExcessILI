fd7aede6a34ca98fe6312f988e0c0f14d6784ae4
Marcus Russi <marcusrussi@gmail.com>
2020-03-28 12:40:12 -0400

    Moved tests into 'testthat' framework. One test still fails

--- tests/testthat/test-ilinet.R
+++ tests/testthat/test-ilinet.R
@@ -1,29 +1,36 @@
-ili.data <- ilinet(region = c("state"))
-ili.data$state <- state.abb[match(ili.data$region, state.name)]
+library(ilinet)
 
-ili.data <- ili.data[, c("state", "week_start", "ilitotal", "total_patients")]
-#ili.data <- ili.data[ili.data$state %in% c("CA", "NY", "WA", "NJ", "CT"), ]
+test_that("ILINet data is fetched and plotted correctly", {
+  ili.data <- ilinet(region = c("state"))
+  ili.data$state <- state.abb[match(ili.data$region, state.name)]
 
-excess_cases1 <-
-  excessCases(ds = ili.data,
-              datevar = "week_start",
-              agevar = "agec",
-              statevar = "state",
-              denom.var = "total_patients",
-              use.syndromes = c("ilitotal"),
-              time.res = "week")
+  ili.data <- ili.data[, c("state", "week_start", "ilitotal", "total_patients")]
+  #ili.data <- ili.data[ili.data$state %in% c("CA", "NY", "WA", "NJ", "CT"), ]
 
-dashboardPlot(excess_cases1)
+  excess_cases1 <-
+    excessCases(ds = ili.data,
+                datevar = "week_start",
+                agevar = "agec",
+                statevar = "state",
+                denom.var = "total_patients",
+                use.syndromes = c("ilitotal"),
+                time.res = "week")
 
-unexplained.cases <-
-  excessExtract(ds = excess_cases1,
-                syndrome = "ilitotal",
-                extract.quantity = "unexplained.cases")
+  dashboardPlot(excess_cases1)
 
-excess.rr <-
-  excessExtract(ds = excess_cases1,
-                syndrome = "ilitotal",
-                extract.quantity = "resid1")
+  unexplained.cases <-
+    excessExtract(ds = excess_cases1,
+                  syndrome = "ilitotal",
+                  extract.quantity = "unexplained.cases")
 
-par(mfrow = c(1, 1))
-matplot(exp(excess.rr[, , 1]), type = "l")
+  excess.rr <-
+    excessExtract(ds = excess_cases1,
+                  syndrome = "ilitotal",
+                  extract.quantity = "resid1")
+
+  par(mfrow = c(1, 1))
+
+  expect_success(
+    matplot(exp(excess.rr[, , 1]), type = "l")
+  )
+})
