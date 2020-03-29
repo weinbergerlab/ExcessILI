15141784b6075c9d33ba5b66025862b118045b78
Marcus Russi <marcusrussi@gmail.com>
2020-03-29 19:40:53 -0400

    Cleaned up all of the code

--- R/functions.R
+++ R/functions.R
@@ -292,27 +292,43 @@ excessCases <-
                'rsv.var')]
   
   if(time.res=='week'){
-    combo2.sub[,datevar] <- lubridate::floor_date(combo2.sub[,datevar], unit='week')
+    combo2.sub[,datevar] <-
+      lubridate::floor_date(combo2.sub[,datevar], unit='week')
   }
   
-  ds2<-reshape_ds(ds2=combo2.sub, sub.statevar=sub.statevar, agevar=agevar, datevar=datevar)
+  ds2 <-
+    reshape_ds(ds2=combo2.sub,
+               sub.statevar=sub.statevar,
+               agevar=agevar,
+               datevar=datevar)
  
-  ages <-   dimnames(ds2)[[3]]
-  geos<-dimnames(ds2)[[2]]
+  ages <- dimnames(ds2)[[3]]
+  geos <- dimnames(ds2)[[2]]
 
-  all.glm.res<- pbapply::pblapply(use.syndromes, function(x){
-    ww<- lapply(ages, function(y){
-      q<-lapply(geos, glm.func, ds=ds2,age.test=y, syndrome=x, denom.var=denom.var, time.res=time.res,extrapolation.date=extrapolation.date)
-      names(q)<-geos
-      return(q)
+  all.glm.results <- pbapply::pblapply(use.syndromes, function(x){
+    f <- function(y) {
+      model.result <- lapply(
+        geos, glm.func,
+        ds        = ds2,
+        age.test  = y,
+        syndrome  = x,
+        denom.var = denom.var,
+        time.res  = time.res,
+        extrapolation.date = extrapolation.date
+      )
+
+      names(model.result) <- geos
+      return(model.result)
     }
-    ) 
-    names(ww)<- ages        
+
+    ww <- lapply(ages, f)
+    names(ww) <- ages        
+
     return(ww)
   })
 
-  names(all.glm.res) <- use.syndromes
-  return(all.glm.res)
+  names(all.glm.results) <- use.syndromes
+  return(all.glm.results)
 }
 
 #' ONE LINER DESCRIPTION
@@ -331,136 +347,197 @@ excessCases <-
 #'
 #' @export
 dashboardPlot <- function(all.glm.res){ 
-  ds <- all.glm.res
+
+  ds               <- all.glm.res
   counties.to.test <- names(ds[[1]][[1]])
-  ages.to.test <- names(ds[[1]])
-  age.labels <- ages.to.test
-  syndromes <- names(ds)
-  dates <- as.Date(ds[[1]][[1]][[1]][['date']])
-  n.times <- length(dates)
+  ages.to.test     <- names(ds[[1]])
+  age.labels       <- ages.to.test
+  syndromes        <- names(ds)
+  dates            <- as.Date(ds[[1]][[1]][[1]][['date']])
+  n.times          <- length(dates)
   last.date.format <- max(dates)
   last.date.format <- format(last.date.format, "%b %d, %Y")
-  
 
-  server<-function(input, output){
+  server <- function(input, output){
     output$countyPlot = renderPlot({
+
+      # Essentially a partialized sapply, one-off for the following few lines
+      plucker <- function(var) function(x) sapply(x, "[[", var, simplify='array')
       
-            ili2.resid <- sapply(ds[[input$set.syndrome]], function(x) sapply(x, 
-                "[[", "resid1"), simplify = "array")
-            dimnames(ili2.resid)[[2]] <- counties.to.test
-            ili2.pred <- sapply(ds[[input$set.syndrome]], function(x) sapply(x, 
-                "[[", "pred"), simplify = "array")
-            dimnames(ili2.pred)[[2]] <- counties.to.test
-            ili2.pred.lcl <- sapply(ds[[input$set.syndrome]], function(x) sapply(x, 
-                "[[", "lpi"), simplify = "array")
-            dimnames(ili2.pred.lcl)[[2]] <- counties.to.test
-            ili2.pred.ucl <- sapply(ds[[input$set.syndrome]], function(x) sapply(x, 
-                "[[", "upi"), simplify = "array")
-            dimnames(ili2.pred.ucl)[[2]] <- counties.to.test
-            obs.ili <- sapply(ds[[input$set.syndrome]], function(x) sapply(x, "[[", 
-                "y"), simplify = "array")
-            dimnames(obs.ili)[[2]] <- counties.to.test
-            denom <- sapply(ds[[input$set.syndrome]], function(x) sapply(x, "[[", 
-                "denom"), simplify = "array")
-            dimnames(denom)[[2]] <- counties.to.test
-            plot.min <- which(input$display.dates == dates)
-            dates.select <- plot.min:n.times
-            par(mfrow = c(2, 3), mar = c(3, 2, 1, 1))
-            
-            if(input$arrange.plots=='Age'){
-              i.select<-input$set.ages
-              j.select<-input$set.borough
-            }else{
-              i.select<-input$set.ages
-              j.select<-counties.to.test
+      data_to_pluck <- ds[[input$set.syndrome]]
+
+      ili2.resid    <- sapply(data_to_pluck, plucker("resid1"))
+      ili2.pred     <- sapply(data_to_pluck, plucker("pred"))
+      ili2.pred.lcl <- sapply(data_to_pluck, plucker("lpi"))
+      ili2.pred.ucl <- sapply(data_to_pluck, plucker("upi"))
+      obs.ili       <- sapply(data_to_pluck, plucker("y"))
+      denom         <- sapply(data_to_pluck, plucker("denom"))
+
+      dimnames(ili2.resid)[[2]]    <- counties.to.test
+      dimnames(ili2.pred)[[2]]     <- counties.to.test
+      dimnames(ili2.pred.lcl)[[2]] <- counties.to.test
+      dimnames(ili2.pred.ucl)[[2]] <- counties.to.test
+      dimnames(obs.ili)[[2]]       <- counties.to.test
+      dimnames(denom)[[2]]         <- counties.to.test
+
+      plot.min <- which(input$display.dates == dates)
+      date.idxs <- plot.min:n.times
+      par(mfrow = c(2, 3), mar = c(3, 2, 1, 1))
+      
+      if(input$arrange.plots=='Age'){
+        i.select<-input$set.ages
+        j.select<-input$set.borough
+      } else {
+        i.select<-input$set.ages
+        j.select<-counties.to.test
+      }
+
+      for(i in i.select){
+        for( j in j.select){
+            if (input$set.prop == "Counts") {
+
+              y <- obs.ili[date.idxs, j, i]
+
+              pred     <- ili2.pred    [date.idxs, j, i]
+              pred.lcl <- ili2.pred.lcl[date.idxs, j, i]
+              pred.ucl <- ili2.pred.ucl[date.idxs, j, i]
+
+              if (identical(input$set.axis, F)) {
+                maxval <-
+                  max(c(ili2.pred.lcl[date.idxs, j, i],
+                        ili2.pred.ucl[date.idxs, j, i],
+                        ili2.pred    [date.idxs, j, i],
+                        obs.ili      [date.idxs, j, i]),
+                      na.rm = T)
+
+                y.range <- c(0, maxval)
+
+              } else {
+                maxval <-
+                  max(c(ili2.pred.lcl[date.idxs, j, ],
+                        ili2.pred.ucl[date.idxs, j, ],
+                        ili2.pred    [date.idxs, j, ],
+                        obs.ili      [date.idxs, j, ]),
+                      na.rm = T)
+
+                y.range <- c(0, maxval)
+
+              }
+            } else if (input$set.prop == "Proportion") {
+              common.denom <- denom[date.idxs, j, i]
+
+              y        <- obs.ili      [date.idxs, j, i] / common.denom
+              pred     <- ili2.pred    [date.idxs, j, i] / common.denom
+              pred.lcl <- ili2.pred.lcl[date.idxs, j, i] / common.denom
+              pred.ucl <- ili2.pred.ucl[date.idxs, j, i] / common.denom
+              
+              if (input$set.axis == F) {
+                y.range <- c(0, max(y, na.rm = T))
+              } else {
+                y.range <- c(0, max(plot.prop[date.idxs, j, ], na.rm = T))
+              }
+            } else {
+              y        <- obs.ili[date.idxs, j, i]/ili2.pred    [date.idxs, j, i]
+              pred     <- obs.ili[date.idxs, j, i]/ili2.pred    [date.idxs, j, i]
+              pred.lcl <- obs.ili[date.idxs, j, i]/ili2.pred.lcl[date.idxs, j, i]
+              pred.ucl <- obs.ili[date.idxs, j, i]/ili2.pred.ucl[date.idxs, j, i]
+
+              if (input$set.axis == F) {
+                y.range <- range(y, na.rm = T)
+                y.range[is.infinite(y.range)] <- 10
+              } else {
+                y.range <- c(0.2, 4)
+              }
             }
-            for(i in i.select){
-              for( j in j.select){
-                  if (input$set.prop == "Counts") {
-                    y = obs.ili[dates.select, j, i]
-                    pred <- ili2.pred[dates.select, j, i]
-                    pred.lcl <- ili2.pred.lcl[dates.select, j, i]
-                    pred.ucl <- ili2.pred.ucl[dates.select, j, i]
-                    if (input$set.axis == F) {
-                      y.range <- c(0, max(c(ili2.pred.lcl[dates.select, j, i], ili2.pred.ucl[dates.select, 
-                        j, i], ili2.pred[dates.select, j, i], obs.ili[dates.select, 
-                        j, i]), na.rm = T))
-                    } else {
-                      y.range <- c(0, max(c(ili2.pred.lcl[dates.select, j, ], ili2.pred.ucl[dates.select, 
-                        j, ], ili2.pred[dates.select, j, ], obs.ili[dates.select, 
-                        j, ]), na.rm = T))
-                    }
-                  } else if (input$set.prop == "Proportion") {
-                    y = obs.ili[dates.select, j, i]/denom[dates.select, j, i]
-                    pred <- ili2.pred[dates.select, j, i]/denom[dates.select, j, 
-                      i]
-                    pred.lcl <- ili2.pred.lcl[dates.select, j, i]/denom[dates.select, 
-                      j, i]
-                    pred.ucl <- ili2.pred.ucl[dates.select, j, i]/denom[dates.select, 
-                      j, i]
-                    
-                    if (input$set.axis == F) {
-                      y.range <- c(0, max(y, na.rm = T))
-                    } else {
-                      y.range <- c(0, max(plot.prop[dates.select, j, ], na.rm = T))
-                    }
-                  } else {
-                    y = obs.ili[dates.select, j, i]/ili2.pred[dates.select, j, i]
-                    pred <- obs.ili[dates.select, j, i]/ili2.pred[dates.select, 
-                      j, i]
-                    pred.lcl <- obs.ili[dates.select, j, i]/ili2.pred.lcl[dates.select, 
-                      j, i]
-                    pred.ucl <- obs.ili[dates.select, j, i]/ili2.pred.ucl[dates.select, 
-                      j, i]
-                    if (input$set.axis == F) {
-                      y.range <- range(y, na.rm = T)
-                      y.range[is.infinite(y.range)] <- 10
-                    } else {
-                      y.range <- c(0.2, 4)
-                    }
-                  }
-                  plot(dates[dates.select], y, type = "n", bty = "l", ylab = "Fitted", 
-                    main = paste(j, i), ylim = y.range)
-                  polygon(c(dates[dates.select], rev(dates[dates.select])), c(pred.lcl, 
-                    rev(pred.ucl)), col = rgb(1, 0, 0, alpha = 0.1), border = NA)
-                  lines(dates[dates.select], pred, type = "l", col = "red", lty = 1, 
-                    lwd = 1.5)
-                  lines(dates[dates.select], y, lwd = 1.5)
-                  if (input$set.prop == "Observed/Expected") {
-                    abline(h = 1, col = "gray", lty = 2)
-                  }
-                }
+
+            plot(dates[date.idxs],
+                 y,
+                 type = "n",
+                 bty  = "l",
+                 ylab = "Fitted", 
+                 main = paste(j, i),
+                 ylim = y.range)
+
+            polygon(c(dates[date.idxs], rev(dates[date.idxs])),
+                    c(pred.lcl, rev(pred.ucl)),
+                    col = rgb(1, 0, 0, alpha = 0.1), 
+                    border = NA)
+
+            lines(dates[date.idxs],
+                  pred,
+                  type = "l",
+                  col = "red",
+                  lty = 1, 
+                  lwd = 1.5)
+
+            lines(dates[date.idxs], y, lwd = 1.5)
+
+            if (input$set.prop == "Observed/Expected") {
+              abline(h = 1, col = "gray", lty = 2)
             }
           }
-    ,
-    width = "auto", height = "auto")
-  }
+      }
+    } ,
+
+    width = "auto",
+    height = "auto"
+  )}
+
+  si <- shiny::selectInput
   
-  ui <- shiny::fluidPage(shiny::titlePanel(paste0('Data through ', last.date.format)), 
-      shiny::sidebarLayout(
-        shiny::sidebarPanel(
-          shiny::selectInput("set.prop", "Proportion of ED visits or count:",
-                      choice=c('Proportion','Counts','Observed/Expected'), selected ="Proportion" ),
-          shiny::selectInput("set.borough", "Geographic unit:",
-                      choice=counties.to.test, selected ="Citywide" ),
-          shiny::selectInput("set.syndrome", "Syndrome:",
-                      choice=syndromes, selected ="ili" ),
-          shiny::sliderInput('display.dates', 'Earliest date to display', min=min(dates), step=7,max=dates[length(dates)-2], value=dates[length(dates)-round(length(dates)/5)]),
-          shiny::checkboxInput("set.axis", "Uniform axis for all plots?:",
-                        value =F ),
-          shiny::selectInput("arrange.plots", "Arrange plots by:",
-                      choice=c('Age','Region'), selected ="Age"),
-          shiny::selectInput("set.ages", "Ages:",
-                      choice=ages.to.test, selected =ages.to.test, multiple=T)
+  ui <- shiny::fluidPage(
+    shiny::titlePanel(paste0('Data through ', last.date.format)), 
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
+        si("set.prop",
+           "Proportion of ED visits or count:",
+           choice=c('Proportion','Counts','Observed/Expected'),
+           selected ="Proportion" ),
+
+        si("set.borough",
+           "Geographic unit:",
+           choice=counties.to.test,
+           selected ="Citywide" ),
+
+        si("set.syndrome",
+           "Syndrome:",
+           choice=syndromes,
+           selected ="ili"),
+
+        shiny::sliderInput(
+          'display.dates',
+          'Earliest date to display',
+          min=min(dates),
+          max=dates[length(dates)-2],
+          step=7,
+          value=dates[length(dates) - round(length(dates)/5)]
         ),
-        shiny::mainPanel(
-          shiny::plotOutput("countyPlot"),
-          shiny::column(8, align = 'justify',
-                 shiny::hr(),
-                 shiny::span("The black line shows the observed number of ED visits per day in the indicated stratum, and the red lines denote the mean and 95% prediction intervals for a model adjusting for seasonality, influenza activity, and RSV activity"),
-                 shiny::hr(),
-                 shiny::span("This app and package were developed by The Public Health Modeling Unit and The Weinberger Lab at Yale School of Public Health. Contributors include Dan Weinberger, Alyssa Amick, Forrest Crawford, Kelsie Cassell, Marcus Russi, Ernest Asare, Yu-Han Kao. Underlying analysis code can be found at https://github.com/weinbergerlab/ExcessILI"),
-                 
+
+        shiny::checkboxInput("set.axis",
+                             "Uniform axis for all plots?:",
+                             value=F),
+
+        si("arrange.plots",
+           "Arrange plots by:",
+           choice=c('Age','Region'),
+           selected ="Age"),
+
+        si("set.ages",
+           "Ages:",
+           choice=ages.to.test,
+           selected =ages.to.test,
+           multiple=T)
+      ),
+
+      shiny::mainPanel(
+        shiny::plotOutput("countyPlot"),
+        shiny::column(
+          8,
+          align = 'justify',
+          shiny::hr(),
+          shiny::span("The black line shows the observed number of ED visits per day in the indicated stratum, and the red lines denote the mean and 95% prediction intervals for a model adjusting for seasonality, influenza activity, and RSV activity"),
+          shiny::hr(),
+          shiny::span("This app and package were developed by The Public Health Modeling Unit and The Weinberger Lab at Yale School of Public Health. Contributors include Dan Weinberger, Alyssa Amick, Forrest Crawford, Kelsie Cassell, Marcus Russi, Ernest Asare, Yu-Han Kao. Underlying analysis code can be found at https://github.com/weinbergerlab/ExcessILI")
           )
         )
       )
@@ -486,7 +563,9 @@ dashboardPlot <- function(all.glm.res){
 #'
 #' @export
 excessExtract <- function(ds, syndrome, extract.quantity) {
-    out.ds <- sapply(ds[[syndrome]], function(x) sapply(x, "[[", extract.quantity), 
-        simplify = "array")
+    out.ds <-
+      sapply(ds[[syndrome]],
+             function(x) sapply(x, "[[", extract.quantity), simplify = "array")
+
     return(out.ds)
 }
